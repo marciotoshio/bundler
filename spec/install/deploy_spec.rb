e77ce068e4966655c4c19a30423fbbed1fb0c83f
David Rodríguez <deivid.rodriguez@riseup.net>
2019-12-23 22:19:31 +0100

    Unify deployment setting and flag
    Previously, we were recommending `bundle config deployment true; bundle
    install` as an alternative to `bundle install --deployment`, but they
    were not working equally.

--- spec/install/deploy_spec.rb
+++ spec/install/deploy_spec.rb
@@ -202,6 +202,12 @@ RSpec.describe "install with --deployment or --frozen" do
       expect(err).not_to include("You have changed in the Gemfile")
     end
 
+    it "installs gems by default to vendor/bundle when `--deployment` is set via an environment variable", :bundler => "< 3" do
+      ENV["BUNDLE_DEPLOYMENT"] = "true"
+      bundle "install"
+      expect(out).to include("vendor/bundle")
+    end
+
     it "can have --frozen set to false via an environment variable" do
       gemfile <<-G
         source "#{file_uri_for(gem_repo1)}"
@@ -312,7 +318,7 @@ RSpec.describe "install with --deployment or --frozen" do
 
   context "when replacing a host with the same host with credentials" do
     before do
-      bundle! "install"
+      bundle! "install", forgotten_command_line_options(:path => "vendor/bundle")
       gemfile <<-G
         source "http://user_name:password@localgemserver.test/"
         gem "rack"
